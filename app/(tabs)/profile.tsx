5069a3ab6919cecb2bf5fb393586c5eabdb14865
abdelilaherretby <abdelilaherretby@gmail.com>
2026-03-09 10:45:34 +0000

    Fresh Expo App with nativewind

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Profilerofile() {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  )
+}
